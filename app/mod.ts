ac595dd8b160c987d19bd1da1e27ee00cd55d357
Samuele De Tomasi <el3um4s@gmail.com>
2021-02-28 20:47:35 +0100

    test

--- app/mod.ts
+++ app/mod.ts
@@ -17,35 +17,35 @@ interface ElementConnected {
   let listConnection:Array<ElementConnected> = [];
 
 
-  for await (const req of serve(`:8090`)) {
-    const { conn, r: bufReader, w: bufWriter, headers } = req;
-    const sock: WebSocket = await acceptWebSocket({ conn, bufReader, bufWriter, headers });
-    handleWs(sock);
-  }
-  
-  
-  async function handleWs(sock: WebSocket): Promise<void> {
-    console.log("socket connected!");
-    try {
-      for await (const ev of sock) {
-        if (typeof ev === "string") {
-          //console.log("ws:Text", ev);
-          const message:SOCKETMessage = convertMessageToObject(ev);
-          await chooseAction(message, sock);
-        } else if (isWebSocketCloseEvent(ev)) {
-          const { code, reason } = ev;
-          console.log("ws:Close", code, reason);
-          Deno.exit();
-        }
-      }
-    } catch (err) {
-      console.error(`failed to receive frame: ${err}`);
-  
-      if (!sock.isClosed) {
-        await sock.close(1000).catch(console.error);
+for await (const req of serve(`:8090`)) {
+  const { conn, r: bufReader, w: bufWriter, headers } = req;
+  const sock: WebSocket = await acceptWebSocket({ conn, bufReader, bufWriter, headers });
+  handleWs(sock);
+}
+
+
+async function handleWs(sock: WebSocket): Promise<void> {
+  console.log("socket connected!");
+  try {
+    for await (const ev of sock) {
+      if (typeof ev === "string") {
+        //console.log("ws:Text", ev);
+        const message:SOCKETMessage = convertMessageToObject(ev);
+        await chooseAction(message, sock);
+      } else if (isWebSocketCloseEvent(ev)) {
+        const { code, reason } = ev;
+        console.log("ws:Close", code, reason);
+        Deno.exit();
       }
     }
+  } catch (err) {
+    console.error(`failed to receive frame: ${err}`);
+
+    if (!sock.isClosed) {
+      await sock.close(1000).catch(console.error);
+    }
   }
+}
   
 interface SOCKETMessage {
     action: string;
@@ -74,37 +74,36 @@ interface SOCKETPlayer {
 
 
 async function chooseAction(message:SOCKETMessage, sock: WebSocket) {
-    const { action, target } = message;
-    if (action === "CLOSE" && target === "ALL") {
-        Deno.exit();
+  const { action, target } = message;
+  if (action === "CLOSE" && target === "ALL") {
+      Deno.exit();
+  }
+  if (action === "TEST") {
+    listConnection.forEach( element => element.sock.send("Hi!"))      
+  }
+  if (action === "CONFIGURE") {
+    if ( message?.option?.config ){
+      const windowName:string = message?.option?.config.windowName;
+      const camera:string = message?.option?.config.camera;
+      listConnection.push({windowName, camera, sock });
     }
-    if (action === "TEST") {
-      listConnection.forEach( element => element.sock.send("Hi!"))      
+  }
+  if (action === "PLAY"){
+    await Deno.run({ cmd: ["./webview/cameras/A/MultiWindows.exe"] });
+    await Deno.run({ cmd: ["./webview/cameras/B/MultiWindows.exe"] });
+    await Deno.run({ cmd: ["./webview/cameras/C/MultiWindows.exe"] });
+    await Deno.run({ cmd: ["./webview/cameras/D/MultiWindows.exe"] });
+  }
+  if (action === "PLAYER POSITION") {
+    if (message?.option?.player) {
+      const sendBack = JSON.stringify(message);
+      listConnection.forEach( element => element.sock.send(sendBack));
     }
-    if (action === "CONFIGURE") {
-      if ( message?.option?.config ){
-        // console.log(message);
-        const windowName:string = message?.option?.config.windowName;
-        const camera:string = message?.option?.config.camera;
-        listConnection.push({windowName, camera, sock });
-      }
-    }
-    if (action === "PLAY"){
-      await Deno.run({ cmd: ["./webview/cameras/A/MultiWindows.exe"] });
-      await Deno.run({ cmd: ["./webview/cameras/B/MultiWindows.exe"] });
-      await Deno.run({ cmd: ["./webview/cameras/C/MultiWindows.exe"] });
-      await Deno.run({ cmd: ["./webview/cameras/D/MultiWindows.exe"] });
-    }
-    if (action === "PLAYER POSITION") {
-      if (message?.option?.player) {
-        const sendBack = JSON.stringify(message);
-        listConnection.forEach( element => element.sock.send(sendBack));
-      }
 
-    }
+  }
 }
 
 function convertMessageToObject(message:string):SOCKETMessage {
-    const obj = JSON.parse(message);
-    return obj;
+  const obj = JSON.parse(message);
+  return obj;
 }
